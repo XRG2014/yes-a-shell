d863c4d4fbfbbcd6d629e6ebd9392e30f37e7c03
XRG2014 <138714694+XRG2014@users.noreply.github.com>
2024-06-01 10:48:04 -0700

    Update yes.py

--- yes.py
+++ yes.py
@@ -1,11 +1,35 @@
 import sys
+from colorama import Fore
  
 n = len(sys.argv)
 
 try:
     f = sys.argv[1]
+    if n > 2 and f == "--help" or n > 2 and f == "--version":
+        print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} Invalid argument(s)')
+        sys.exit()
+    elif f == "--help":
+        print("Usage: yes [STRING]...")
+        print("  or:  yes OPTION")
+        print("Repeatedly output a line with all specified STRING(s), or 'y'.")
+        print("      --help        display this help and exit")
+        print("      --version     output version information and exit")
+        sys.exit()
+    elif f == "--version":
+        print("yes 2.x")
+        print("Packaged by XRG2014 (2.x)")
+        print("Written by XRG2014")
+        sys.exit()
+    elif f.startswith("-") and not f == "--version" and not f == "--help":
+        print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} Invalid argument(s)')
+        sys.exit()
 except IndexError:
-    sys.exit()
+    try:
+        while True:
+            print("y", end = " ")
+            print()
+    except KeyboardInterrupt:
+        sys.exit()
 
 try:
     while True:
